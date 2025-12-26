5af67adb788cd245d08493527a8a5dceab460562
burhaninegol-xup <burhaninegol@gmail.com>
2025-12-26 21:41:13 +0300

    kullanılmayan dosyaları sildik

--- İpragaz Bayi_files/acc.ipra.regex.js
+++
@@ -1,7 +0,0 @@
-ACC.ipraRegex = {
-    number: '/[0-9]/',
-    phoneNumber: '^\\d{3}\\s{1}\\d{3}\\s{1}\\d{4}$',
-    replaceSpace: function (text) {
-        return text.replace(/ /g, "");
-    }
-};
--- İpragaz Bayi_files/acc.ipra.stylesheet.js
+++
@@ -1,9 +0,0 @@
-ACC.ipraStyleSheet = {
-    workSchedule: function () {
-        document.styleSheets[1].addRule('.switch__button::before', 'content: "' + ACC.ipraMessage.off + '";');
-        document.styleSheets[1].addRule('.switch__button::after', 'content: "' + ACC.ipraMessage.on + '";');
-    }
-};
-with (ACC.ipraStyleSheet) {
-    workSchedule();
-}
